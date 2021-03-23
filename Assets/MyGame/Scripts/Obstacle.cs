8492b5db7c178dee88e28e5fe2e50bb4303647d8
jenngla <jennifer.glantschnig@htl-salzburg.ac.at>
2021-03-23 10:33:34 +0100

    deleted void Start

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -12,11 +12,6 @@ private void Awake()
     rb = GetComponent<Rigidbody2D>();
 }
 
-// Start is called before the first frame update
-void Start()
-{
-        
-}
 
 // Update is called once per frame
 void Update()
